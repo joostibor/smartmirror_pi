8b955453b439bd41132def8729064e1d2bb0e8c4
Krisiiii98 <43846045+Krisiiii98@users.noreply.github.com>
2019-12-03 22:26:10 +0100

    Update buttonpresspicture.py
    Programkód finomítás

--- buttonpresspicture.py
+++ buttonpresspicture.py
@@ -1,13 +1,12 @@
-# needed modules will be imported
 import RPi.GPIO as GPIO
 import time
   
 GPIO.setmode(GPIO.BCM)
   
-# The input pin of the Sensor will be declared. Additional to that the pullup resistor will be activated.
-LED_Rot = 17
-LED_Gruen = 27
-LED_Blau = 22
+# Pin deklarálás
+LED_red = 17
+LED_green = 27
+LED_blue = 22
 GPIO_PIN = 24
 GPIO.setup(GPIO_PIN, GPIO.IN, pull_up_down = GPIO.PUD_UP)
 GPIO.setup(LED_Rot, GPIO.OUT) 
@@ -15,46 +14,45 @@ GPIO.setup(LED_Gruen, GPIO.OUT)
 GPIO.setup(LED_Blau, GPIO.OUT)
 Freq = 100
 
-ROT = GPIO.PWM(LED_Rot, Freq) 
-GRUEN = GPIO.PWM(LED_Gruen, Freq)
-BLAU = GPIO.PWM(LED_Blau, Freq)
-ROT.start(0)  
-GRUEN.start(0)
-BLAU.start(0)
+RED = GPIO.PWM(LED_red, Freq) 
+GREEN = GPIO.PWM(LED_green, Freq)
+BLUE = GPIO.PWM(LED_blue, Freq)
+RED.start(0)  
+GREEN.start(0)
+BLUE.start(0)
 
-def LED_Farbe(Rot, Gruen,Blau, pause):
-    ROT.ChangeDutyCycle(Rot)
-    GRUEN.ChangeDutyCycle(Gruen)
-    BLAU.ChangeDutyCycle(Blau)
+def LED_Color(Red, Green,Blue, pause):
+    RED.ChangeDutyCycle(Red)
+    GREEN.ChangeDutyCycle(Green)
+    BLUE.ChangeDutyCycle(Blue)
     time.sleep(pause)
  
-    ROT.ChangeDutyCycle(0)
-    GRUEN.ChangeDutyCycle(0)
+    RED.ChangeDutyCycle(0)
+    GREEN.ChangeDutyCycle(0)
   
 print "Sensor-Test [press ctrl+c to end it]"
   
-# This output function will be started at signal detection
+# Jelérzékelés és képkészítés
 def outFunction(null):
         print("Signal detected")
-        LED_Farbe(1,0,0,.02)
+        LED_Color(1,0,0,.02)
         import time
         import os
         n = 10
-        while n>0: #enter your value here
+        while n>0:
             print(n)
-            time.sleep(1) #to wait a second
+            time.sleep(1) #1 sec várakozás
             n = n-1
         os.system('sh jpgmake.sh')
   
-# At the moment of detecting a Signal ( falling signal edge ) the output function will be activated.
 GPIO.add_event_detect(GPIO_PIN, GPIO.FALLING, callback=outFunction, bouncetime=100) 
   
-# main program loop
+# main program
 try:
         while True:
                 time.sleep(1)
   
-# Scavenging work after the end of the program
+# Program végén tisztítás
 except KeyboardInterrupt:
         GPIO.cleanup()
 
