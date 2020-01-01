9f75b57f6f3cf9ff305644c7832082f2ef1a11d9
Krisiiii98 <43846045+Krisiiii98@users.noreply.github.com>
2020-01-01 19:45:16 +0100

    Mozgásérzékelő beépítése a programba

--- buttonpresspicture.py
+++ buttonpresspicture.py
@@ -7,13 +7,17 @@ GPIO.setmode(GPIO.BCM)
 LED_red = 17
 LED_green = 27
 LED_blue = 22
-GPIO_PIN = 24
-GPIO.setup(GPIO_PIN, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+BUTTON_PIN = 24
+MOTION_PIN = 25
+# Pin setup
+GPIO.setup(BUTTON_PIN, GPIO.IN, pull_up_down = GPIO.PUD_UP)
 GPIO.setup(LED_red, GPIO.OUT) 
 GPIO.setup(LED_green, GPIO.OUT)
 GPIO.setup(LED_blue, GPIO.OUT)
+GPIO.setup(MOTION_PIN, GPIO.IN) 
 Freq = 100
 
+# LED szinek default
 RED = GPIO.PWM(LED_red, Freq) 
 GREEN = GPIO.PWM(LED_green, Freq)
 BLUE = GPIO.PWM(LED_blue, Freq)
@@ -22,81 +26,96 @@ GREEN.start(0)
 BLUE.start(0)
 
 def LED_Farbe(Red, Green, Blue, pause):
-    RED.ChangeDutyCycle(red)
-    GREEN.ChangeDutyCycle(green)
-    BLUE.ChangeDutyCycle(blue)
+    RED.ChangeDutyCycle(Red)
+    GREEN.ChangeDutyCycle(Green)
+    BLUE.ChangeDutyCycle(Blue)
     time.sleep(pause)
  
     RED.ChangeDutyCycle(0)
     GREEN.ChangeDutyCycle(0)
     BLUE.ChangeDutyCycle(0)
-  
-print "Sensor-Test"
-# Okostukor program inditas
-import os
-os.system('sh smstart.sh')
 
-
-# Jelerzekeles
+# Gombnyomas erzekelese
 def outFunction(null):
-        print("Signal detected")
-        LED_Farbe(1,0,0,.02)
-        import time
-        n = 10
-        while n>0:
-		if n>3:
-                	LED_Farbe(0,1,0,.500)
-                	print n
-                if n<4:
-                	LED_Farbe(1,0,0,.500)
-                	print n
-            time.sleep(1) # Varakozasi ido
-            n = n-1
-        # foto+twitter poszt
-        import tweepy  
-        from twython import Twython
-        from keys import (
-         consumer_key,
-         consumer_secret,
-         access_token,
-         access_token_secret
-        )
-        os.system('sh jpgmake.sh')
-         
-        # Kulcsok es tokenek hasznalata 
-        auth = tweepy.OAuthHandler(consumer_key, consumer_secret)  
-        auth.set_access_token(access_token, access_token_secret)  
-           
-        api = tweepy.API(auth)  
-          
-        # Foto feltoltese 
-        photo_path = '/home/pi/Documents/Projekt/tmp.jpg'
-	# random szoveg a twitter poszthoz
-        messages =  [
-                     "Hogy vagytok?",
-                     "Milyen a napotok?",
-                     "It's me Mario!!!",
-                     "Randomly access league control.",
-                     "Be nice person!",
-                     "Arccal a pacalba!",
-                    ]
-        import random
-        message = random.choice(messages)
-        status = message
-        api.update_with_media(photo_path, status=status)
-        print "Posztolva"
-	
-	# tmp kep torlese
-        os.system('sh jpgremove.sh')
-        print "Temp kep torolve"
- 
-GPIO.add_event_detect(GPIO_PIN, GPIO.FALLING, callback=outFunction, bouncetime=100) 
-  
-# main program
+    print("Signal detected")
+    LED_Farbe(0,0,1,.500)
+    n = 10
+    while n>0:
+        if n>3:
+            LED_Farbe(0,1,0,.500)
+            print n
+        if n<4:
+            LED_Farbe(1,0,0,.500)
+            print n
+        time.sleep(1)
+        n = n-1
+    # foto+twitter poszt
+    import tweepy  
+    from twython import Twython
+    from keys import (
+            consumer_key,
+            consumer_secret,
+            access_token,
+            access_token_secret
+    )
+    os.system('sh jpgmake.sh')
+                         
+    # Kulcsok es tokenek hasznalata 
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)  
+    auth.set_access_token(access_token, access_token_secret)  
+                           
+    api = tweepy.API(auth)  
+                          
+    # Foto feltoltese 
+    photo_path = '/home/pi/Documents/Projekt/tmp.jpg'
+    # random szoveg a twitter poszthoz
+    messages =  [
+                    "Hogy vagytok?",
+                    "Milyen a napotok?",
+                    "It's me Mario!!!",
+                    "Randomly access league control.",
+                    "Be nice person!",
+                    "Arccal a pacalba!",
+                ]
+    import random
+    message = random.choice(messages)
+    status = message
+    api.update_with_media(photo_path, status=status)
+    print "Posztolva"
+                    
+    # tmp kep torlese
+    os.system('sh jpgremove.sh')
+    print "Temp kep torolve"
+GPIO.add_event_detect(BUTTON_PIN, GPIO.FALLING, callback=outFunction, bouncetime=100)
+
+run = False # fut-e mar a MagicMirror
+    
 try:
-        while True:
-                time.sleep(1)
-  
+    time.sleep(1)
+    while True:
+        if GPIO.input(MOTION_PIN) and run==False:
+            print("Motion detected")
+            # Okostukor program inditas
+            import os
+            os.system('sh smstart.sh')
+            
+            run = True
+            time.sleep(3)
+            # timer a MagicMirror kikapcsolasahoz 1 perc tetlenseg utan OFF
+            x=0
+            while x<61:
+                if x%5==0:
+                    print(x)
+                if GPIO.input(MOTION_PIN) and run==True:
+                    x=0
+                if x==60:
+                    os.system('./probakill.sh')
+                    time.sleep(1)
+                    run = False
+                time.sleep(1) 
+                x=x+1
+            time.sleep(30) #fel percig nem kapcsol vissza a tukor
+
 # Program vegen tisztitas
 except KeyboardInterrupt:
         GPIO.cleanup()
