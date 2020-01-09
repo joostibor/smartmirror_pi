19333eb21f1bc6a288c7d6ce0b7a6a58bf524809
joostibor <43845789+joostibor@users.noreply.github.com>
2020-01-09 17:23:48 +0100

    Update jpgmake.sh
    Finomítások

--- jpgmake.sh
+++ jpgmake.sh
@@ -2,4 +2,5 @@
 
 DATE=$(date +"%Y-%m-%d_%H%M")
 
-raspistill -t 1 -o /home/pi/Documents/Projekt/Pictures/$DATE.jpg
+raspistill -t 3000  -vf -st -br 50 -n -o /home/pi/Documents/Projekt/Pictures/$DATE.jpg
+raspistill -t 3000  -vf -st -br 50 -n -o /home/pi/Documents/Projekt/tmp.jpg
