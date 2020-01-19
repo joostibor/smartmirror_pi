053458073cce79108a9528d0788c0ec7c727cd58
joostibor <43845789+joostibor@users.noreply.github.com>
2020-01-19 19:12:50 +0100

    Update jpgmake.sh

--- jpgmake.sh
+++ jpgmake.sh
@@ -2,5 +2,5 @@
 
 DATE=$(date +"%Y-%m-%d_%H%M")
 
-raspistill -t 3000  -vf -st -br 50 -n -o /home/pi/Documents/Projekt/Pictures/$DATE.jpg
-raspistill -t 3000  -vf -st -br 50 -n -o /home/pi/Documents/Projekt/tmp.jpg
+raspistill -t 3000  -rot 270 -st -br 50 -n -o /home/pi/Documents/Projekt/Pictures/$DATE.jpg
+raspistill -t 3000  -rot 270 -st -br 50 -n -o /home/pi/Documents/Projekt/tmp.jpg
